e58993eebdc17047169279f4409ea01db305606f
kres0v <kresov@yahoo.com>
2022-12-02 19:06:22 +0400

    1

---
+++ js/utils.js
@@ -0,0 +1,28 @@
+var utils = (function ($) {
+  var $window = $(window);
+  function isMobile() {
+    return (
+      $window.width() < 40 * 16 ||
+      /Android|webOS|iPhone|iPod|iPad|BlackBerry|IEMobile/i.test(navigator.userAgent)
+    );
+  }
+  function createNodes(list) {
+    var result = {};
+    for (key in list) {
+      result[key] = $(list[key]);
+    }
+    return result;
+  }
+  return { createNodes: createNodes, isMobile: isMobile };
+})(jQuery);
+
+var GodTouch = (function ($) {
+  var touch = $('#touch');
+  $(window).scroll(() => {
+    if (window.scrollY > 2200) {
+      touch.show();
+    } else {
+      touch.hide();
+    }
+  });
+})(jQuery);
